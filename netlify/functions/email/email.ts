85a8848a4461d5728786639814efe40c0673d120
Chid Gilovitz <chid.gilovitz@gmail.com>
2021-05-18 21:51:50 +0800

    Update email sending function to send csv as attachment

--- netlify/functions/email/email.ts
+++ netlify/functions/email/email.ts
@@ -1,17 +1,45 @@
 import { Handler } from "@netlify/functions"
 import formData from "form-data"
 import Mailgun from "mailgun.js"
+import { createObjectCsvStringifier } from "csv-writer"
+
 const mailgun = new Mailgun(formData)
 
 const sendEmail = async ({ user, data }) => {
   return new Promise((resolve, reject) => {
     const mg = mailgun.client({ username: "api", key: process.env.MG_API_KEY })
 
+    const csvStringifier = createObjectCsvStringifier({
+      header: [
+        { id: "number", title: "Number" },
+        { id: "notes", title: "Notes" },
+        { id: "fieldName", title: "Field Name" },
+        { id: "location", title: "Location" },
+        { id: "time", title: "Time" },
+      ],
+    })
+
+    const records = Object.keys(data).map((key) => {
+      const item = data[key]
+      return {
+        ...item,
+        number: `${user.initials}${item.number}`,
+        location: `${item.location?.latitude}, ${item.location?.longitude}`,
+        time: new Date(Date.parse(item.timestamp)),
+      }
+    })
+
     const mailData = {
       from: `Fieldbook <${process.env.FROM_EMAIL}>`,
       to: [user.email],
       subject: "Data from Fieldbook",
       text: JSON.stringify(data),
+      attachment: [
+        {
+          data: Buffer.from(csvStringifier.stringifyRecords(records), "base64"),
+          filename: `fieldbook-${user.initials}-${Date.now()}`,
+        },
+      ],
     }
 
     return mg.messages
