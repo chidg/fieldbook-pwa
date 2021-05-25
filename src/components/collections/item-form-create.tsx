5c73a8045339e0024cf2526d4b928d2134f4204b
Chid Gilovitz <chid.gilovitz@gmail.com>
2021-05-25 13:01:48 +0800

    Testing in prod

--- src/components/collections/item-form-create.tsx
+++ src/components/collections/item-form-create.tsx
@@ -60,6 +60,7 @@ export const ItemFormCreate: React.FC = () => {
       initialValues={initialValues()}
       onSubmit={(values) => {
         console.log('saving lo', geoLocation)
+        console.log('coords ?', geoLocation?.coords)
         saveItem({ ...values, id: v4(), timestamp: Date.now(), location: geoLocation?.coords })
         history.replace('/')
       }}
