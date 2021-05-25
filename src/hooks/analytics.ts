845faa65ab0f1a2f54f3d7183502511c05295131
Chid Gilovitz <chid.gilovitz@gmail.com>
2021-05-25 22:00:05 +0800

    Set user id in GA if available

--- src/hooks/analytics.ts
+++ src/hooks/analytics.ts
@@ -1,15 +1,23 @@
 import React from "react"
 import ReactGA from "react-ga"
 import { useLocation } from "react-router-dom"
+import { useUserContext } from "../contexts"
 
 export const useGoogleAnalytics = () => {
   const location = useLocation()
+  const { user } = useUserContext()
 
   React.useEffect(() => {
     if (process.env.REACT_APP_GA_TRACKING_ID) {
       ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID)
     }
   }, [])
+
+  React.useEffect(() => {
+    ReactGA.set({
+      userId: user?.email,
+    })
+  }, [user?.email])
 
   React.useEffect(() => {
     const currentPath = location.pathname + location.search
