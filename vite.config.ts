7836e35b6c7579d5f698682f8078def7f7b432fc
Chid Gilovitz <chid@talisman.xyz>
2023-12-06 08:25:44 +0800

    fix: manifest

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,34 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    VitePWA({ registerType: "autoUpdate" }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "Fieldbook",
+        name: "Fieldbook for NCMRR",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+    }),
     pluginChecker({ typescript: true }),
   ],
 })
