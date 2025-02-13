b29007941c86a69e563c6dc279ef7141c26d4f1e
Chid Gilovitz <chid@talisman.xyz>
2025-02-13 18:34:09 +0800

    chore: delete compiled tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import "./styles/tailwind.css"
+import "./styles/index.css"
 import App from "./App"
 import * as Sentry from "@sentry/react"
 import * as SentryBrowser from "@sentry/browser"
